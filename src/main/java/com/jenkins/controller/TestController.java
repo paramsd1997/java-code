285b8ed97fa08cddc8454aed924b1e63698f4c46
paramsd1997 <111410267+paramsd1997@users.noreply.github.com>
2022-08-27 22:58:04 +0530

    Update TestController.java

--- src/main/java/com/jenkins/controller/TestController.java
+++ src/main/java/com/jenkins/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-@GetMapping("/")
+@GetMapping("/abc")
 public String health() {
   return "Hello & Welcome to Java Springboot !!!";
 }
